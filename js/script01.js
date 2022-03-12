c7cd16054e7163ed5a8873565e2fa6473944628a
qkrtmdwn <qxtse130613@naver.com>
2022-03-12 14:43:42 +0900

    all files upload

--- js/script01.js
+++ js/script01.js
@@ -1,32 +1,54 @@
 $(function(){
-    //alert("aaa");
-
-    //슬라이드 메뉴
-    $('.nav li').hover(
+    $('.menu>ul>li').hover(
         function(){
-            $('.nav .sub').stop(true,true).slideDown();
+            $('.menu .sub').stop().slideDown();
         },
         function(){
-            $('.nav .sub').stop(false,false).slideUp();
+            $('.menu .sub').stop().slideUp();
         }
     );
+    
+     //팝업
+     $('.tab_content>div>ul>li').click(
+         function(){
+            $('#popup').fadeIn();
+        }
+    );
+    $('.pop>.button').click(
+        function(){
+           $('#popup').fadeOut();
+       }
+   );
 
-    //슬라이드 이미지
+   //텝메뉴
+    var tab = $('.tab ul li');
+    var content = $('.tab_content>div');
+    content.hide().eq(0).show();
+    tab.click(function(event){
+        event.preventDefault();
+        var tg =  $(this);
+        var i  = tg.index();
+        tab.removeClass('active');
+        tg.addClass('active');
+        content.css('display','none');
+        content.eq(i).css('display','block');
+    });
+
+    //슬라이드
     var banner = $('#banner ul li');
     var current = 0;
     var timer;
 
-    function move(tg, start, end, op1, op2){
-        tg.css({left:start,opacity:op1}).stop().animate({left:end,opacity:op2},500);
+    function move(tg,start,end){
+        tg.css('top',start).stop().animate({top:end},500);
     };
-
     timer=setInterval(function(){
         var prev = banner.eq(current);
-        move(prev, '0%', '-100%', 1,0);
+        move(prev,'0%','-100%');
         current++;
-        if(current == banner.size()){current = 0;};
+        if(current == banner.size()){current =0;};
         var next = banner.eq(current);
-        move(next, '100%', '0%', 0,1);
+        move(next,'100%','0%');
     },1000);
     banner.hover(
         function(){
@@ -35,35 +57,12 @@ $(function(){
         function(){
             timer=setInterval(function(){
                 var prev = banner.eq(current);
-                move(prev, '0%', '-100%', 1,0);
+                move(prev,'0%','-100%');
                 current++;
-                if(current == banner.size()){current = 0;};
+                if(current == banner.size()){current =0;};
                 var next = banner.eq(current);
-                move(next, '100%', '0%', 0,1);
+                move(next,'100%','0%');
             },1000);
         }
     );
-
-    //텝메뉴
-    var tab = $('.tab ul li');
-    var content = $('.tab_content>div');
-    content.hide();
-    content.eq(0).show();
-    tab.click(function(e){
-        e.preventDefault();
-        var tg = $(this);
-        var i = tg.index();
-        tab.removeClass('active');
-        tg.addClass('active');
-        content.css('display','none');
-        content.eq(i).css('display','block');
-    });
-
-    //팝업창
-    $('.popup').click(function(){
-        $('#pop').fadeIn();
-    });
-    $('.button').click(function(){
-        $('#pop').fadeOut();
-    });
 });
